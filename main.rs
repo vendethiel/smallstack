51f2edc80889dcdff3e3dd7c26b2075857d24e22
vendethiel <vendethiel@hotmail.fr>
2015-05-05 10:27:04 +0200

    Hi I'm trying out rust

---
+++ main.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+struct VM {
+  stack: Vec<String>
+}
+
+fn main() {
+	if let Some(path) = env::args().nth(1) {
+		println!("Loading file {}", path);
+
+    match File::open(path.to_string()) {
+      Ok(file) => {
+        let mut file = file;
+        let mut content = String::new();
+        if let Ok(_) = file.read_to_string(&mut content) {
+          println!("The file contains {}", content);
+        }
+      },
+      Err(err) => {
+        println!("Can't read file {} (error: {})", path, err);
+      }
+    }
+	} else {
+    println!("Please give a file argument");
+  }
+
+  //     let path = Path::new("chry.fa");
+  // let mut file = BufferedReader::new(File::open(&path));
+  // for line in file.lines() {
+  //  print!("{}", line.unwrap());
+  //      }
+}
