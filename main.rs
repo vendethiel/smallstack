ff669322181fb0d6bce12aad35fb56cc71b37893
vendethiel <vendethiel@hotmail.fr>
2015-05-05 17:58:08 +0200

    Trying to work with vectors, i64, etc etc etc

--- main.rs
+++ main.rs
@@ -1,9 +1,10 @@
+#![feature(slice_patterns)]
 use std::env;
 use std::fs::File;
 use std::io::Read;
 
 struct VM<'a> {
-  stack: Vec<String>,
+  stack: Vec<i64>,
   instructions: Vec<&'a str>
 }
 
@@ -14,20 +15,36 @@ impl<'a> VM<'a> {
       instructions: instructions
     }
   }
+
+  pub fn run(&self) {
+    // TODO pass in default stack arguments?
+    for instr in self.instructions.iter() {
+      match instr.split(" ") {
+        ["push", n] => self.stack.push(try!(n.parse::<i64>())),
+        ["add"] => {
+          if let Some(arg1) = self.stack.pop() && Some(arg2) = self.stack.pop() {
+            println!("got {}", arg1 + arg2);
+          } else {
+            println!("VM error: not enough arguments to `add`");
+          }
+        },
+        [..] => println!("VM error: Unrecognized instruction!") // todo err!
+      }
+    }
+  }
 }
 
 fn main() {
 	if let Some(path) = env::args().nth(1) {
 		println!("Loading file {}", path);
 
-    match File::open(path.to_string()) {
+    match File::open(&path) {
       Ok(file) => {
         let mut file = file;
         let mut content = String::new();
         if let Ok(_) = file.read_to_string(&mut content) {
-          println!("The file contains {}", content);
-
           let vm = VM::new(content.split('\n').collect());
+          vm.run();
         }
       },
       Err(err) => {
