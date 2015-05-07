f34343f769eb670a26356e8e4ea5c1bae92a9020
vendethiel <vendethiel@hotmail.fr>
2015-05-07 09:50:54 +0200

    I swear mutability is okay.

--- main.rs
+++ main.rs
@@ -16,19 +16,19 @@ impl<'a> VM<'a> {
     }
   }
 
-  pub fn run(&self) {
+  pub fn run(&mut self) {
     // TODO pass in default stack arguments?
     for instr in self.instructions.iter() {
-      match instr.split(" ") {
-        ["push", n] => self.stack.push(try!(n.parse::<i64>())),
+      match instr.split(" ").collect::<Vec<_>>().as_slice() {
+        ["push", n] => self.stack.push(n.parse::<i64>().unwrap()),
         ["add"] => {
-          if let Some(arg1) = self.stack.pop() && Some(arg2) = self.stack.pop() {
+          if let (Some(arg1), Some(arg2)) = (self.stack.pop(), self.stack.pop()) {
             println!("got {}", arg1 + arg2);
           } else {
             println!("VM error: not enough arguments to `add`");
           }
         },
-        [..] => println!("VM error: Unrecognized instruction!") // todo err!
+        _ => println!("VM error: Unrecognized instruction!") // todo err!
       }
     }
   }
@@ -39,11 +39,10 @@ fn main() {
 		println!("Loading file {}", path);
 
     match File::open(&path) {
-      Ok(file) => {
-        let mut file = file;
+      Ok(mut file) => {
         let mut content = String::new();
         if let Ok(_) = file.read_to_string(&mut content) {
-          let vm = VM::new(content.split('\n').collect());
+          let mut vm = VM::new(content.split('\n').collect());
           vm.run();
         }
       },
